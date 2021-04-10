d7a3d380d2089a5e8f0270d1a40eb23570b18cde
Paolo Derchi <derchipaolo73@gmail.com>
2021-04-10 18:59:23 +0200

    Citta repository

---
+++ algoritmorsa/src/main/java/com/elaborato/algoritmorsa/repository/CittaRepository.java
@@ -0,0 +1,8 @@
+package com.elaborato.algoritmorsa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.elaborato.algoritmorsa.model.Citta;
+
+public interface CittaRepository extends JpaRepository<Citta, Integer>{
+}
